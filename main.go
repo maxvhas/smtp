aed2018daeaeb376dc10bd333080e65e6768c50c
Hugo Thunnissen <devel@hugot.nl>
2023-03-12 16:56:12 +0100

    Fix build of main.go

--- main.go
+++ main.go
@@ -1,27 +1,12 @@
 package main
 
 import (
-	"net"
+	"log"
 
-	log "github.com/sirupsen/logrus"
 	"snorba.art/mail/inbound/smtp"
 )
 
 func main() {
-	laddr, err := net.ResolveTCPAddr("tcp", "127.0.0.1:1025")
-	l, err := net.ListenTCP("tcp", laddr)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	for {
-		conn, err := l.Accept()
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		log.Info("got connection")
-
-		go smtp.HandleIncoming(conn, nil)
-	}
+	_, err := smtp.Listen("127.0.0.1:1025", smtp.Config{})
+	log.Fatal(err)
 }
